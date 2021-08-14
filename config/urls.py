b01c216f8426bde0424fadb20063968cbeeaa827
ShahzodAhadov <88456020+ShahzodAhadov@users.noreply.github.com>
2021-08-14 16:06:03 +0500

    s

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('appnomi.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('',include('appnomi.urls')),
 
 ]
